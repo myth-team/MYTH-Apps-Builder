9bebba5d3333dbefee6759dd3e2aff83ce7f717c
myth-team <myth-team@users.noreply.github.com>
2026-03-31 17:15:18 +0000

    Deploying to gh-pages from @ myth-team/MYTH-Apps-Builder@03dd7339ca522fdcd921fadccc808150e73a1dd7 🚀

--- previews/project-69cbf612bde416945ddeceab/flutter_service_worker.js
+++ previews/project-69cbf612bde416945ddeceab/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"manifest.json": "3fc29f9294b755bca0378065b89e216e",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/fonts/MaterialIcons-Regular.otf": "3af142fc09c4c8b89cc0cc3abb81ed74",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
-"flutter_bootstrap.js": "b44112feab24cfd95200ef696bb46df7",
+"flutter_bootstrap.js": "4f24712bde5e690a053f95202c046cd7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "27b54091e1372a5ed99f818f0e948e84"};
 // The application shell files that are downloaded before a service worker can
