3d27fe60826d9b8b72610f7b5a7312f8ee4fd66a
myth-team <myth.team.ai@gmail.com>
2026-04-04 20:24:59 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69d12cc21e17bf000a578e9f)

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_master_app/utils/colors.dart'; 
+import 'package:tictactoe_master_app/screens/home_screen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Tic Tac Toe Master',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
