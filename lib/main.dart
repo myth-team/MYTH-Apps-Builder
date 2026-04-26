3d1370fc2729335288b661ee68ebe0875bdec399
myth-team <myth.team.ai@gmail.com>
2026-04-26 23:27:42 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69ee58cd25139cef29b5a02d)

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:secure_entry_app/utils/colors.dart'; 
+import 'package:secure_entry_app/screens/login_screen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Secure Entry App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          surface: AppColors.surface,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
