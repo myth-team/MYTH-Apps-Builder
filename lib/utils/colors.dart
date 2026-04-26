feeb244db976021fb3c6a9ae76bca30b341db9bb
myth-team <myth.team.ai@gmail.com>
2026-04-27 00:07:18 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69ee624735f6b6a4b8acd999)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = Color(0xFFFF6B35);
+  static Color secondary = Color(0xFF004E64);
+  static Color accent = Color(0xFFFF9F1C);
+  static Color background = Color(0xFFFFFDF9);
+  static Color surface = Color(0xFFFFFFFF);
+  static Color textPrimary = Color(0xFF2D3436);
+  static Color textSecondary = Color(0xFF636E72);
+  static Color success = Color(0xFF00B894);
+}
