53112f418358a6a7c92c0d4bb75b6dfe0bfa9688
myth-team <myth-team@users.noreply.github.com>
2026-04-27 20:01:10 +0000

    Deploying to gh-pages from @ myth-team/MYTH-Apps-Builder@ed7611537ad48662d24ef8d4b864fb2379f239dd 🚀

--- previews/project-69efadfa0c429e201ac48d11/flutter_bootstrap.js
+++ previews/project-69efadfa0c429e201ac48d11/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2490811106"
+    serviceWorkerVersion: "2513179101"
   }
 });
