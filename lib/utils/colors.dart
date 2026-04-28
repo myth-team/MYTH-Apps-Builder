0e595acc6a1ea5a1346cf5fe044ca53ac2ee5448
myth-team <myth.team.ai@gmail.com>
2026-04-28 22:23:01 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69f0e575e01d8d5f44a4b537)

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryColor = Color(0xFF4A90E2);
-  static const secondaryColor = Color(0xFF50E3C2);
+  static const primaryColor = Color(0xFF27AE60);
+  static const secondaryColor = Color(0xFF2ECC71);
   static const backgroundColor = Color(0xFFF5F5F5);
   static const cardColor = Colors.white;
   static const textColor = Colors.black;
