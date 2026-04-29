e8ade56d033db5cab3deba986ec30a9bda0b8711
myth-team <myth.team.ai@gmail.com>
2026-04-29 22:50:08 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f23f42dcacfcd66560d0b6)

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class TapCounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
-      title: 'Tap Count',
+      title: 'Tap Countd',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
