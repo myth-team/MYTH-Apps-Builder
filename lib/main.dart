2e3b6be0336e4a205af94be87416b3e14c2077cc
myth-team <myth.team.ai@gmail.com>
2026-04-30 01:26:25 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f2691522ffcd7b506072cf)

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:taskflow_daily_app/screens/home_screen.dart'; 
+import 'package:taskflow_daily_app/utils/colors.dart'; 
+
+void main() {
+  runApp(const TaskFlowApp());
+}
+
+class TaskFlowApp extends StatelessWidget {
+  const TaskFlowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'TaskFlow',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        fontFamily: 'Roboto',
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
