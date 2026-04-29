d20e0d3946e70a4dc273598ef413dedde3625376
myth-team <myth.team.ai@gmail.com>
2026-04-29 12:51:07 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f1b3f538cca74449466bfc)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zee_luxury_jewels_app/utils/colors.dart'; 
+import 'package:zee_luxury_jewels_app/providers/cart_provider.dart';
+import 'package:zee_luxury_jewels_app/providers/wishlist_provider.dart';
 import 'package:zee_luxury_jewels_app/screens/splash_screen.dart'; 
 
 void main() {
@@ -8,15 +12,20 @@ void main() {
 class ZeeLuxuryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
-      title: 'Zee Luxury',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        scaffoldBackgroundColor: Color(0xFF0A0A0A),
-        fontFamily: 'PlayfairDisplay',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => WishlistProvider()),
+      ],
+      child: MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+        title: 'Zee Luxury',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          scaffoldBackgroundColor: AppColors.deepBlack,
+        ),
+        home: SplashScreen(),
       ),
-      home: SplashScreen(),
     );
   }
 }
