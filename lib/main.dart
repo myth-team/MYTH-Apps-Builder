98810c8f3838af1b90894ba7386342eeb46db200
myth-team <myth.team.ai@gmail.com>
2026-04-30 02:33:36 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f26acd19adfff9958389bf)

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MemeStreamApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
-      title: 'MemeStream',
+      title: 'DaudMeme',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: AppColors.primary,
