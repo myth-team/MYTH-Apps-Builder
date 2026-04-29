8d11e788fadec37f0fe0ac23d2ffb23d02469cfb
myth-team <myth.team.ai@gmail.com>
2026-04-29 20:30:12 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f2239e8b2a9df62257d1e2)

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:new_project_app/screens/login_screen.dart'; 
+import 'package:new_project_app/utils/colors.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'New Project App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          surface: AppColors.surface,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+        useMaterial3: true,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
