3df661e70d8c14a9daf918041899314608eec8d9
myth-team <myth.team.ai@gmail.com>
2026-04-30 01:50:51 +0500

    Update lib/widgets/loading_shimmer.dart via MYTH Build System (Branch: project-69f26acd19adfff9958389bf)

--- lib/widgets/loading_shimmer.dart
+++ lib/widgets/loading_shimmer.dart
@@ -14,4 +14,88 @@ class _LoadingShimmerState extends State<LoadingShimmer>
   late Animation<double> _animation;
 
   @override
-  void initState
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    )..repeat();
+    _animation = Tween<double>(begin: -2, end: 2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOutSine),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Container(
+          width: double.infinity,
+          height: 400,
+          decoration: BoxDecoration(
+            color: AppColors.cardBackground,
+            borderRadius: BorderRadius.circular(24),
+            border: Border.all(
+              color: AppColors.primary.withOpacity(0.3),
+              width: 2,
+            ),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(22),
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment(_animation.value, 0),
+                      end: Alignment(_animation.value + 1, 0),
+                      colors: [
+                        AppColors.shimmer,
+                        AppColors.cardBackground.withOpacity(0.5),
+                        AppColors.shimmer,
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 16,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: AppColors.shimmer,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      height: 12,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: AppColors.shimmer,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
