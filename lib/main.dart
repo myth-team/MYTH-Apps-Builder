1bfd6e40b55e55b5ae294ee6dc8296cc049a4bf0
myth-team <myth.team.ai@gmail.com>
2026-04-30 00:53:12 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f26185ddbb88bfa28bc6e5)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:simply_count_app/utils/colors.dart'; 
+import 'package:simply_count_app/screens/home_screen.dart'; 
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: Center(child: Text('MYTH Warmup')),
-    ),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Simply Count',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
 }
