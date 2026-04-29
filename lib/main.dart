d9fd208da3f6c2dc13c3969b0a002b81718bd166
myth-team <myth.team.ai@gmail.com>
2026-04-29 22:26:59 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f23f42dcacfcd66560d0b6)

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:tap_counter_app/utils/colors.dart'; 
+import 'package:tap_counter_app/screens/counter_screen.dart'; 
+
+void main() {
+  runApp(const TapCounterApp());
+}
+
+class TapCounterApp extends StatelessWidget {
+  const TapCounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Tap Counter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          surface: AppColors.surface,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+        useMaterial3: true,
+      ),
+      home: const CounterScreen(),
+    );
+  }
+}
