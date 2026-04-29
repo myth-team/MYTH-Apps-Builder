1ae0d6d718f0fb4ebcbff61f3b672853c4663333
myth-team <myth.team.ai@gmail.com>
2026-04-30 02:25:11 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f2772f2bdbf19bfeed16e3)

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:new_project_app/utils/colors.dart'; 
+import 'package:new_project_app/screens/onboarding/welcome_screen.dart'; 
+import 'package:new_project_app/screens/onboarding/auth_screen.dart'; 
+import 'package:new_project_app/screens/onboarding/wallet_created_screen.dart'; 
+import 'package:new_project_app/screens/main_navigation.dart'; 
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Web3 Neobank',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        textTheme: GoogleFonts.interTextTheme(),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
