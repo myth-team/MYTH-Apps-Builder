45ecf6fbd23d78d72b604a14a3014bf4a1eed16d
myth-team <myth.team.ai@gmail.com>
2026-04-30 02:25:13 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69f2772f2bdbf19bfeed16e3)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary brand colors
+  static Color primary = Color(0xFF6C5CE7);
+  static Color primaryLight = Color(0xFF8B7ED8);
+  static Color primaryDark = Color(0xFF5A4FCF);
+  
+  // Background colors
+  static Color background = Color(0xFFF8F9FA);
+  static Color surface = Color(0xFFFFFFFF);
+  static Color cardBackground = Color(0xFFF1F3F5);
+  
+  // Text colors
+  static Color textPrimary = Color(0xFF212529);
+  static Color textSecondary = Color(0xFF6C757D);
+  static Color textLight = Color(0xFF9CA3AF);
+  
+  // Action colors
+  static Color success = Color(0xFF00D4AA);
+  static Color warning = Color(0xFFFFC107);
+  static Color error = Color(0xFFFF6B6B);
+  static Color info = Color(0xFF74B9FF);
+  
+  // Transaction colors
+  static Color deposit = Color(0xFF00D4AA);
+  static Color spending = Color(0xFFFF6B6B);
+  static Color yield = Color(0xFF6C5CE7);
+  static Color investment = Color(0xFF74B9FF);
+  
+  // Card colors
+  static Color cardPrimary = Color(0xFF1A1A1A);
+  static Color cardText = Color(0xFFFFFFFF);
+  
+  // Border colors
+  static Color border = Color(0xFFE9ECEF);
+  static Color borderLight = Color(0xFFF1F3F5);
+}
