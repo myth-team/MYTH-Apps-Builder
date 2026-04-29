b7ba70c69b5575a4231ab1af54c0b10d5717b507
myth-team <myth.team.ai@gmail.com>
2026-04-30 04:32:11 +0500

    Update lib/widgets/transaction_list.dart via MYTH Build System (Branch: project-69f2772f2bdbf19bfeed16e3)

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:web_3_banking_forked_app/utils/colors.dart'; 
+import 'package:new_project_app/utils/colors.dart'; 
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions = [
