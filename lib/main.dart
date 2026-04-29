c6f28aaa0485ef36a728a73282012905ee24f9b5
myth-team <myth.team.ai@gmail.com>
2026-04-29 11:57:16 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f1ab5e69fd636a5ade5576)

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_glow_app/utils/colors.dart'; 
+import 'package:tic_tac_glow_app/screens/home_screen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Tic Tac Glow',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
