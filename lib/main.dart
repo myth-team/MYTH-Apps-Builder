9332aeaf52cd361bf5df0cb63f6968908f46b3ff
myth-team <myth.team.ai@gmail.com>
2026-04-30 05:30:38 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f29f35c111000d4275a843)

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hello_connect_app/utils/colors.dart'; 
+import 'package:hello_connect_app/screens/counter_screen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Counter App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+      ),
+      home: const CounterScreen(),
+    );
+  }
+}
