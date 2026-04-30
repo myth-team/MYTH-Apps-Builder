658b64c4951ba9e843b6c48c2830c6d2568334cb
myth-team <myth.team.ai@gmail.com>
2026-05-01 04:22:56 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69f3e415bfbefa2f40803f45)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6C63FF);
+  static const Color secondaryColor = Color(0xFFFF6584);
+  static const Color backgroundColor = Color(0xFFF8F9FE);
+  static const Color cardBackgroundColor = Color(0xFFFFFFFF);
+  static const Color textPrimaryColor = Color(0xFF2D3436);
+  static const Color textSecondaryColor = Color(0xFF636E72);
+  static const Color incrementColor = Color(0xFF00B894);
+  static const Color decrementColor = Color(0xFFE17055);
+  static const Color resetColor = Color(0xFF74B9FF);
+  static const Color shadowColor = Color(0x1A000000);
+  static const Color iconColor = Color(0xFF2D3436);
+  static const Color positiveColor = Color(0xFF00B894);
+  static const Color negativeColor = Color(0xFFE17055);
+  static const Color neutralColor = Color(0xFF636E72);
+  static const Color accentGradientStart = Color(0xFF6C63FF);
+  static const Color accentGradientEnd = Color(0xFFFF6584);
+  static const Color buttonHighlight = Color(0xFF5A52D9);
+  static const Color buttonPressed = Color(0xFF4A42B8);
+  static const Color shimmerBase = Color(0xFFE0E0E0);
+  static const Color shimmerHighlight = Color(0xFFF5F5F5);
+  static const Color errorColor = Color(0xFFD63031);
+  static const Color successColor = Color(0xFF00B894);
+  static const Color warningColor = Color(0xFFFDCB6E);
+  static const Color infoColor = Color(0xFF74B9FF);
+  static const Color dividerColor = Color(0xFFDFE6E9);
+  static const Color overlayColor = Color(0x80000000);
+  static const Color disabledColor = Color(0xFFB2BEC3);
+  static const Color rippleColor = Color(0x296C63FF);
+}
