d6d8af4be331774ef20b185feb2039738e504961
myth-team <myth.team.ai@gmail.com>
2026-04-30 21:12:19 +0500

    Update lib/utils/colors.dart via MYTH Build System (Branch: project-69f37d90375e3c5741de9216)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = Color(0xFF6C63FF);
+  static Color primaryLight = Color(0xFF8B85FF);
+  static Color primaryDark = Color(0xFF5249E0);
+  static Color secondary = Color(0xFFFF6B6B);
+  static Color accent = Color(0xFFFF9F43);
+  static Color success = Color(0xFF2ED573);
+  static Color warning = Color(0xFFFFBB33);
+  static Color error = Color(0xFFFF4757);
+  static Color background = Color(0xFFF8F9FE);
+  static Color surface = Color(0xFFFFFFFF);
+  static Color card = Color(0xFFFFFFFF);
+  static Color textPrimary = Color(0xFF1A1D26);
+  static Color textSecondary = Color(0xFF6B7280);
+  static Color textTertiary = Color(0xFF9CA3AF);
+  static Color divider = Color(0xFFE5E7EB);
+  static Color shadow = Color(0x1A000000);
+  static Color overlay = Color(0x80000000);
+  static Color white = Color(0xFFFFFFFF);
+  static Color black = Color(0xFF000000);
+  static Color grey100 = Color(0xFFF3F4F6);
+  static Color grey200 = Color(0xFFE5E7EB);
+  static Color grey300 = Color(0xFFD1D5DB);
+  static Color grey400 = Color(0xFF9CA3AF);
+  static Color grey500 = Color(0xFF6B7280);
+}
