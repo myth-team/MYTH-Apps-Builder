f5b996ce24faad503d2f1a338b11e457981f1ed1
myth-team <myth.team.ai@gmail.com>
2026-04-30 20:52:25 +0500

    Update lib/main.dart via MYTH Build System (Branch: project-69f379a03a820c59f53cb065)

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:simple_counter_pro_app/screens/home.dart'; 
+import 'package:simple_counter_pro_app/utils/colors.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Simple Counter Pro',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          primary: AppColors.primary,
+          secondary: AppColors.secondary,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
