3ceab315ea76838000cd08725a568c4d2c9260b3
myth-team <myth.team.ai@gmail.com>
2026-05-07 02:23:55 +0500

    Update lib/screens/categories.dart via MYTH Build System (Branch: project-69fa690c0040e025a3b15e76)

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -26,7 +26,7 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Categories'),
